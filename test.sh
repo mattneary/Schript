5c32657623412ab0bbc098a8ba227927ce6db0e8
Matt Neary <neary.matt@gmail.com>
2013-08-17 19:21:58 -0400

    very rough implementation of internal macros

--- test.sh
+++ test.sh
@@ -8,3 +8,5 @@ node lib/schript.js ../tests/conds.sch | \
 	tests/assert.sh 5 "conditionals"	
 node lib/schript.js ../tests/recursion.sch | \
 	tests/assert.sh 24 "recursion"			
+node lib/schript.js ../tests/macros.sch | \
+	tests/assert.sh 56 "internal macros"				
